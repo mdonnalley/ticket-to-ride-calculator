c0ae5473351e445532d1702ba3672d70a4b8f07e
Mike Donnalley <mdonnalley@icxmedia.com>
2019-02-17 18:34:42 -0700

    Add rankings table and longest train selector

---
+++ src/components/LongestTrain/LongestTrain.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+
+export default class LongestTrain extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selection : ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const playerName = event.target.value;
+    this.setState({ selection: playerName});
+    this.props.handleLongestTrain(playerName);
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Typography color='primary' variant='h6'>Longest Train</Typography>
+        <Select
+          value={this.state.selection}
+          onChange={this.handleChange}
+          inputProps={{ name: 'longest-train', id: 'longest-train' }}
+          displayEmpty>
+          <MenuItem value='' disabled>
+            Select Player with Longest Train
+          </MenuItem>
+          {
+            this.props.players.map(player => {
+              return <MenuItem key={player.name} value={player.name}>{player.name}</MenuItem>
+            })
+          }
+        </Select>
+      </React.Fragment>
+      
+    );
+  }
+
+}
